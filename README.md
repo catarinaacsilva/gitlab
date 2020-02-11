8684bd6d7326383917d29f5e9dcab488c084fb7a
Catarina Silva <c.alexandracorreia@ua.pt>
2020-02-11 16:37:06 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Deploy GitLab in a Docker swarm
+
+You can easily configure and deploy your Docker-based GitLab installation in a swarm cluster.
+
+
+## Requirements
+
+- Docker
+
+- [Docker compose](https://docs.docker.com/compose/install/)
+
+
+## Install GitLab using docker-compose
+
+1. Create a Docker-compose.yml
+
+2. 
+
+**Potencial problema:** ACLs
+
+
+## Setup a Docker swarm
+
+[Docker swarm](https://docs.docker.com/engine/swarm/swarm-tutorial/)
+[docker swarm](https://docs.gitlab.com/omnibus/docker/#run-the-image)
+
+
+## Authors
+
+* **Catarina Silva** - [catarinaacsilva](https://github.com/catarinaacsilva)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ gitlab.rb
@@ -0,0 +1,27 @@
+external_url 'https://gitlab.example.com'
+
+# Prevent GitLab from starting if NFS data mounts are not available
+high_availability['mountpoint'] = '/var/opt/gitlab/git-data'
+
+# Disable components that will not be on the GitLab application server
+roles ['application_role']
+nginx['enable'] = true
+
+# PostgreSQL connection details
+gitlab_rails['db_adapter'] = 'postgresql'
+gitlab_rails['db_encoding'] = 'unicode'
+gitlab_rails['db_host'] = '10.1.0.5' # IP/hostname of database server
+gitlab_rails['db_password'] = 'DB password'
+
+# Redis connection details
+gitlab_rails['redis_port'] = '6379'
+gitlab_rails['redis_host'] = '10.1.0.6' # IP/hostname of Redis server
+gitlab_rails['redis_password'] = 'Redis Password'
+
+# Ensure UIDs and GIDs match between servers for permissions via NFS
+user['uid'] = 9000
+user['gid'] = 9000
+web_server['uid'] = 9001
+web_server['gid'] = 9001
+registry['uid'] = 9002
+registry['gid'] = 9002
