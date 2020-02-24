e9dc007da77b02f8603d00405ef38521828fe8de
Catarina Silva <c.alexandracorreia@ua.pt>
2020-02-24 00:15:11 +0000

    Update user class

--- scripts_test/gitlab_user.py
+++ scripts_test/gitlab_user.py
@@ -13,11 +13,26 @@ class GitlabUser:
         # use the username/password authentication.
         # self.gl.auth()
 
-    # create a new user
-    def create_user(self):
-        user_data = {'email':  self.name + '@girs.com', 'username': self.name,
+        # check if the user already exists
+        if self.user_exists():
+            # retrieves the user
+            self.user = self.gl.users.list(username=self.name)[0]
+        else:
+            # create a new user
+            user_data = {'email':  self.name + '@girs.com', 'username': self.name,
                      'name': self.name, 'password': self.name + self.name}
-        self.user = self.gl.users.create(user_data)
+            self.user = self.gl.users.create(user_data)
+
+    # check if the user already exists
+    def user_exists(self):
+        user = self.gl.users.list(username=self.name)[0]
+        if user != None:
+            return True
+        return False
+    
+    # list all users
+    def get_all_users(self):
+        return self.gl.user.list()
 
     # create a new project
     def create_prj(self, project_name: str):
