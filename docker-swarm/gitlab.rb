9d287d8e339435006442d67bdccec52b202ccbe4
Catarina Silva <c.alexandracorreia@ua.pt>
2020-04-30 00:31:09 +0100

    Update gitlab.rb and stack bind

--- docker-swarm/gitlab.rb
+++ docker-swarm/gitlab.rb
@@ -12,28 +12,31 @@ redis_exporter['enable'] = false
 grafana['enable'] = false
 
 # Postgres settings
-gitlab_rails['db_adapter'] = "postgresql"
-gitlab_rails['db_encoding'] = "unicode"
+gitlab_rails['db_adapter'] = 'postgresql'
+gitlab_rails['db_encoding'] = 'utf8'
 
 # database service will be named "postgres" in the stack
-gitlab_rails['db_host'] = "postgres" 
-gitlab_rails['db_database'] = "gitlab"
-gitlab_rails['db_username'] = "gitlab"
-gitlab_rails['db_password'] = "gitlab"
+gitlab_rails['db_host'] = 'postgres'
+gitlab_rails['db_database'] = 'gitlab'
+gitlab_rails['db_username'] = 'gitlab'
+gitlab_rails['db_password'] = 'gitlab'
+
+# Enable or disable automatic database migrations
+#gitlab_rails['auto_migrate'] = false
 
 # Redis settings
 # redis service will be named "redis" in the stack
 gitlab_rails['redis_host'] = "redis"
 
 # Prometheus exporters
-#node_exporter['listen_address'] = '0.0.0.0:9100'
-#gitlab_exporter['listen_address'] = '0.0.0.0'
-#gitaly['prometheus_listen_addr'] = "0.0.0.0:9236"
-#gitlab_workhorse['prometheus_listen_addr'] = "0.0.0.0:9229"
-#sidekiq['listen_address'] = '0.0.0.0'
-#gitlab_exporter['listen_port'] = '9168'
-
 node_exporter['listen_address'] = '0.0.0.0:9100'
-gitlab_monitor['listen_address'] = '0.0.0.0'
+gitlab_exporter['listen_address'] = '0.0.0.0'
 gitaly['prometheus_listen_addr'] = "0.0.0.0:9236"
 gitlab_workhorse['prometheus_listen_addr'] = "0.0.0.0:9229"
+sidekiq['listen_address'] = '0.0.0.0'
+gitlab_exporter['listen_port'] = '9168'
+
+#node_exporter['listen_address'] = '0.0.0.0:9100'
+#gitlab_monitor['listen_address'] = '0.0.0.0'
+#gitaly['prometheus_listen_addr'] = "0.0.0.0:9236"
+#gitlab_workhorse['prometheus_listen_addr'] = "0.0.0.0:9229"
